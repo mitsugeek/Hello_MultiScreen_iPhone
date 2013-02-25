e652a1b065d4528bb17d62fb1ab797cd5311f5f8
mitsugeek <mitsu322@gmail.com>
2013-02-26 07:57:21 +0900

    HomeScreen edit
    and projecy clean

--- Screen/HomeScreen.cs
+++ Screen/HomeScreen.cs
@@ -9,6 +9,9 @@ namespace Hello_MultiScreen_iPhone
 {
 	public partial class HomeScreen : UIViewController
 	{
+		HelloWorldScreen helloWorldScreen;
+		HelloUniverseScreen helloUniverseScreen;
+
 		public HomeScreen () : base ("HomeScreen", null)
 		{
 		}
@@ -26,6 +29,25 @@ namespace Hello_MultiScreen_iPhone
 			base.ViewDidLoad ();
 			
 			// Perform any additional setup after loading the view, typically from a nib.
+			//---- when the hello world button is clicked
+			this.btnHelloWorld.TouchUpInside += (sender, e) => {
+				//---- instantiate a new hello world screen, if it's null
+				// (it may not be null if they've navigated backwards
+				if(this.helloWorldScreen == null)
+				{ this.helloWorldScreen = new HelloWorldScreen(); }
+				//---- push our hello world screen onto the navigation
+				//controller and pass a true so it navigates
+				this.NavigationController.PushViewController(this.helloWorldScreen, true);
+			};
+			
+			//---- same thing, but for the hello universe screen
+			this.btnHelloUniverse.TouchUpInside += (sender, e) => {
+				if(this.helloUniverseScreen == null) {
+					this.helloUniverseScreen = new HelloUniverseScreen();
+				} 
+				
+				this.NavigationController.PushViewController(this.helloUniverseScreen, true);
+			};
 		}
 	}
 }
